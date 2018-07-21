093762dcad7b17b760d5343e6d067836e1a3de74
mrfodon <mrfodon12@gmail.com>
2018-07-21 13:34:31 -0300

    valor a ser financiado

--- teste1.py
+++ teste1.py
@@ -7,3 +7,10 @@ while cont < 12:
     cont += 1
     print("R$%6.2f" % soma)
 print("Valor final -  R$%6.2f" % soma)
+
+#Planejando a compra de um carro
+
+carro = int(input("Valor do carro - R$ "))
+entrada = soma
+financiamento = carro - entrada
+print("Valor a financiar R$%7.2f " % (financiamemto)
