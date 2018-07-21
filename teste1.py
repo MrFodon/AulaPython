90a70deaa7c5a04b055b65b8d3db954faf89d4cd
mrfodon <mrfodon12@gmail.com>
2018-07-21 13:20:25 -0300

    alterando o percentual de rentabilidade

--- teste1.py
+++ teste1.py
@@ -2,7 +2,7 @@ cont = 0
 soma = 0
 while cont < 12:
     mes = float(input("Investimento -  %i° mês R$: " % (cont + 1)))
-    rent = mes * (1 + 0.00857)
+    rent = mes * (1 + 0.005) #taxa base na cardenete de poupança
     soma = soma + rent 
     cont += 1
     print("R$%6.2f" % soma)
